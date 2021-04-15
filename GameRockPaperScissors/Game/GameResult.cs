ddb4d72d8ab94876f4e873a657d18bcc16b09bf3
Ionut <ionut.lazurca@yahoo.com>
2021-04-15 18:07:33 +0300

    Add project files.

---
+++ GameRockPaperScissors/Game/GameResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameRockPaperScissors.Game
+{
+    public enum GameResult
+    {
+        Victory, Loss, Draw
+    }
+}
---
+++ GameRockPaperScissors/Game/Hand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameRockPaperScissors.Game
+{
+    public class Hand
+    {
+        public RockPaperScissors Selection { get; set; }
+        public RockPaperScissors WinAgainst { get; set; }
+        public RockPaperScissors LoseAgainst { get; set; }
+        public string ImageUrl { get; set; }
+
+        public GameResult PlayAgainst(Hand opponent)
+        {
+            if(Selection == opponent.Selection)
+            {
+                return GameResult.Draw;
+            }
+            else if(LoseAgainst == opponent.Selection)
+            {
+                return GameResult.Loss;
+            }
+            return GameResult.Victory;
+        }
+    }
+}
